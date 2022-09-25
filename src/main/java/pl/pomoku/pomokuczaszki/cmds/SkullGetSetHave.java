ec200d5f35391a9345497348b00000d5eacc9dd8
HubiRto <57270888+HubiRto@users.noreply.github.com>
2022-09-25 19:54:35 +0200

    Dodanie uprawnien na dane komendy.

--- src/main/java/pl/pomoku/pomokuczaszki/cmds/SkullGetSetHave.java
+++ src/main/java/pl/pomoku/pomokuczaszki/cmds/SkullGetSetHave.java
@@ -1,13 +1,13 @@
 package pl.pomoku.pomokuczaszki.cmds;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import pl.pomoku.pomokuczaszki.Api;
 import pl.pomoku.pomokuczaszki.PomokuCzaszki;
-import pl.pomoku.pomokuczaszki.ymls_config.KillsYml;
 
 import java.util.Objects;
 
@@ -22,40 +22,56 @@ public class SkullGetSetHave implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if(args.length == 0){
-            Player p = (Player) sender;
-            p.sendMessage("Lista komend:");
-            p.sendMessage("/czaszak have [player] - pokazuje czy gracz ma czaszke");
-            p.sendMessage("/czaszak remove [player] - usuwa graczowi czaszke");
-            p.sendMessage("/czaszak set [player] - daje graczowi czaszke");
+            if(sender.hasPermission("czaszki.all") || sender.hasPermission("czaszki.listakomend")) {
+                Player p = (Player) sender;
+                p.sendMessage("Lista komend:");
+                p.sendMessage("/czaszak have [player] - pokazuje czy gracz ma czaszke");
+                p.sendMessage("/czaszak remove [player] - usuwa graczowi czaszke");
+                p.sendMessage("/czaszak set [player] - daje graczowi czaszke");
+            }else {
+                sender.sendMessage(ChatColor.RED + "Nie masz uprawnien do tej komendy!");
+            }
         }else if(args.length == 2){
             Player target = Bukkit.getPlayer(args[1]);
             if(target != null) {
                 switch (args[0]) {
                     case "have":
-                        if (Api.haveSkull(target)) {
-                            sender.sendMessage("Gracz " + target.getDisplayName() + " ma czaszke.");
-                        } else {
-                            sender.sendMessage("Gracz " + target.getDisplayName() + " nie ma czaszki.");
+                        if(sender.hasPermission("czaszki.all") || sender.hasPermission("czaszki.have")) {
+                            if (Api.haveSkull(target)) {
+                                sender.sendMessage("Gracz " + target.getDisplayName() + " ma czaszke.");
+                            } else {
+                                sender.sendMessage("Gracz " + target.getDisplayName() + " nie ma czaszki.");
+                            }
+                        }else {
+                            sender.sendMessage(ChatColor.RED + "Nie masz uprawnien do tej komendy!");
                         }
                         break;
                     case "remove":
-                        if(Api.haveSkull(target)){
-                            Api.setSkull(target, false);
-                            target.setPlayerListName(target.getName());
-                            sender.sendMessage("Usunales czaszke graczowi " + target.getDisplayName());
-                            target.sendMessage("Gracz " + sender.getName() + " usunal ci czaszke.");
+                        if(sender.hasPermission("czaszki.all") || sender.hasPermission("czaszki.remove")) {
+                            if (Api.haveSkull(target)) {
+                                Api.setSkull(target, false);
+                                target.setPlayerListName(target.getName());
+                                sender.sendMessage("Usunales czaszke graczowi " + target.getDisplayName());
+                                target.sendMessage("Gracz " + sender.getName() + " usunal ci czaszke.");
+                            } else {
+                                sender.sendMessage("Nie mozesz tego zrobic, bo gracz nie posiada czaszki");
+                            }
                         }else {
-                            sender.sendMessage("Nie mozesz tego zrobic, bo gracz nie posiada czaszki");
+                            sender.sendMessage(ChatColor.RED + "Nie masz uprawnien do tej komendy!");
                         }
                         break;
                     case "set":
-                        if(Api.haveSkull(target)){
-                            sender.sendMessage("Ten gracz juz posiada czaszke.");
+                        if(sender.hasPermission("czaszki.all") || sender.hasPermission("czaszki.set")) {
+                            if (Api.haveSkull(target)) {
+                                sender.sendMessage("Ten gracz juz posiada czaszke.");
+                            } else {
+                                Api.setSkull(target, true);
+                                sender.sendMessage("Dodales graczowi " + target.getDisplayName() + " czaszke.");
+                                target.sendTitle("☠", "", 0, 20 * 2, (int) (20 * 0.5));
+                                target.setPlayerListName(target.getName() + " ☠");
+                            }
                         }else {
-                            Api.setSkull(target, true);
-                            sender.sendMessage("Dodales graczowi " + target.getDisplayName() + " czaszke.");
-                            target.sendTitle("☠", "", 0, 20 * 2, (int) (20 * 0.5));
-                            target.setPlayerListName(target.getName() + " ☠");
+                            sender.sendMessage(ChatColor.RED + "Nie masz uprawnien do tej komendy!");
                         }
                 }
             }
--- src/main/java/pl/pomoku/pomokuczaszki/cmds/SkullGuiCmd.java
+++ src/main/java/pl/pomoku/pomokuczaszki/cmds/SkullGuiCmd.java
@@ -1,5 +1,6 @@
 package pl.pomoku.pomokuczaszki.cmds;
 
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -21,7 +22,13 @@ public class SkullGuiCmd implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if(sender instanceof Player p){
-            new SkullMenu(PomokuCzaszki.getPlayerMenuUtility(p)).open();
+            if(sender.hasPermission("czaszki.all") || sender.hasPermission("czaszki.gui")) {
+                new SkullMenu(PomokuCzaszki.getPlayerMenuUtility(p)).open();
+            }else {
+                sender.sendMessage(ChatColor.RED + "Nie masz uprawnien do tej komendy!");
+            }
+        }else {
+            sender.sendMessage(ChatColor.RED + "Tej komendy nie mozna wykonac w konsoli!");
         }
         return true;
     }
